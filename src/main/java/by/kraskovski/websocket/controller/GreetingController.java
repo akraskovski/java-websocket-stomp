a1e6490f0c4841f50f81895c6d83bc0ecaad2f23
akraskovski <artem.kraskovski@gmail.com>
2017-06-17 20:34:05 +0300

    js test functions created

--- src/main/java/by/kraskovski/websocket/controller/GreetingController.java
+++ src/main/java/by/kraskovski/websocket/controller/GreetingController.java
@@ -4,6 +4,7 @@ import by.kraskovski.websocket.domain.Greeting;
 import by.kraskovski.websocket.domain.HelloMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -11,8 +12,8 @@ public class GreetingController {
 
     @MessageMapping("/hello")
     @SendTo("topic/greetings")
+    //@Scheduled(fixedDelay = 1000)
     public Greeting greeting(HelloMessage message) throws InterruptedException {
-        Thread.sleep(1000);
         return new Greeting("Hello, " + message.getName());
     }
 }
