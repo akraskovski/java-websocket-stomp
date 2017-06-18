26b1becd6741ae964431a460511b0750b747df77
akraskovski <artem.kraskovski@gmail.com>
2017-06-18 16:30:03 +0300

    simplified structure

--- src/main/java/by/kraskovski/websocket/controller/GreetingController.java
+++ src/main/java/by/kraskovski/websocket/controller/GreetingController.java
@@ -4,16 +4,15 @@ import by.kraskovski.websocket.domain.Greeting;
 import by.kraskovski.websocket.domain.HelloMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class GreetingController {
 
-    @MessageMapping("/hello")
+    @MessageMapping("/websocket-example")
     @SendTo("topic/greetings")
-    //@Scheduled(fixedDelay = 1000)
     public Greeting greeting(HelloMessage message) throws InterruptedException {
+        Thread.sleep(1000);
         return new Greeting("Hello, " + message.getName());
     }
 }
--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -13,7 +13,7 @@ function setConnected(connected) {
 }
 
 function connect() {
-    var socket = new SockJS('/websocket');
+    var socket = new SockJS('/websocket-example'); //registry endpoint
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
         setConnected(true);
